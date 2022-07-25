799f2cff74fe72c88425cfdbc26539d8ac25c745
zinzin <zinzin.vung@gmail.com>
2022-07-25 21:44:00 +0700

    update

--- inflastructure/database/conn.js
+++ inflastructure/database/conn.js
@@ -3,14 +3,14 @@ const config = require('../../config/appconfig');
 const { env } = config.db
 
 
-const sequelize = new Sequelize('deidqukskpsftr', 'bxsjnsbkfwjmmx', 'f77455dbfc0f2c9eeeae518f89625d32a8f9a361221df147fc58585f17f9cc92', {
-    host: 'ec2-44-208-88-195.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    define:{
-        underscored: true,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-    }
-  });
+const sequelize = new Sequelize('sbrbabst', 'sbrbabst', 'FOfAajwuZjVtxKag0ECUiMawYjZjQa1m', {
+  host: 'kandula.db.elephantsql.com',
+  dialect: 'postgres',
+  define:{
+      underscored: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+  }
+});
 
 module.exports = sequelize
